967692ee813847533dc11fe41a1b690f586b6c2a
Andy <andu_niculae@yahoo.com>
2019-12-19 15:33:24 +0200

    Added Dark mode. TODO: Fix dark mode colors Style dark mode button

---
+++ assets/js/check-mode.js
@@ -0,0 +1,7 @@
+const theme = localStorage.getItem('theme');
+	if (theme === "dark") {
+		document.documentElement.setAttribute('data-theme', 'dark');
+}
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+}
